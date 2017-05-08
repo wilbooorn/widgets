4f9ac7a57aba7e4038ffb620937cf5706aa0b2cb
Robin <robinwilborn@Robins-MacBook-Pro-2.local>
2017-05-07 21:09:00 -0700

    updare autocomplete widget

--- frontend/autocomplete.jsx
+++ frontend/autocomplete.jsx
@@ -3,41 +3,20 @@ import React from "react";
 class AutoComplete extends React.Component {
   constructor(props){
     super(props);
-    this.state = { inputVal: "", names: this.props.names};
-
-    this.search = this.search.bind(this);
-    this.set = this.set.bind(this);
-    this.updateNames = this.updateNames.bind(this);
+    this.state = { inputVal: "" };
   }
 
-  updateNames(){
-    let newNames = this.props.names.filter((name) => {
-      return name.startsWith(this.state.inputVal);
-    })
-    this.setState({ names: newNames });
-  }
-
-  search(e){
-    e.preventDefault();
-    let input = document.getElementById("search").value;
-    this.setState({ inputVal: input }, this.updateNames);
-
-  }
-
-  set(e){
-    e.preventDefault();
-    this.setState({ inputVal: e.target.innerHTML })
+  search(){
+    
   }
 
 
   render(){
     return (
-      <div className = "auto">
-        <input onChange = {this.search} type="text" id="search" value={this.state.inputVal} placeholder="Search..."></input>
+      <div>
+        <input onChange = {this.search} type="text" value={this.state.inputVal} placeholder="Search..."></input>
         <ul>
-          {
-            this.state.names.map((name, idx) => <li onClick = {this.set} key={idx}>{name}</li>)
-          }
+
         </ul>
       </div>
     );
